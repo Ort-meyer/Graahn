8521670fca641a7f87a494cc6d17bab42ef6124c
Azoooka <putte_dt@hotmail.se>
2017-07-04 19:19:23 +0200

    some basic stuff. Also made assets text based

--- Assets/Assets/Scripts/PolygonGenerator.cs
+++ Assets/Assets/Scripts/PolygonGenerator.cs
@@ -19,7 +19,30 @@ public class PolygonGenerator : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        mesh = GetComponent<MeshFilter>().mesh;
 
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float z = transform.position.z;
+
+
+        newVertices.Add(new Vector3(x, y, z));
+        newVertices.Add(new Vector3(x + 1, y, z));
+        newVertices.Add(new Vector3(x + 1, y - 1, z));
+        newVertices.Add(new Vector3(x, y - 1, z));
+
+        newTriangles.Add(0);
+        newTriangles.Add(1);
+        newTriangles.Add(3);
+        newTriangles.Add(1);
+        newTriangles.Add(2);
+        newTriangles.Add(3);
+
+        mesh.Clear();
+        mesh.vertices = newVertices.ToArray();
+        mesh.triangles = newTriangles.ToArray();
+        // mesh.Optimize(); no longer supported apparently
+        mesh.RecalculateNormals();
     }
 
     // Update is called once per frame
